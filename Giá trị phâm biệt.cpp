f57d632e51acfc880a7a74f2afd00acf571e4583
Công Duy Đoàn <congduy2108@gmail.com>
2024-10-05 12:23:45 +0700

    Create Giá trị phâm biệt.cpp

---
+++ Giá trị phâm biệt.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    set<int> distinct_values;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        distinct_values.insert(x);
+    }
+    cout << distinct_values.size() << endl;
+    return 0;
+}
